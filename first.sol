f44d573ed10da213dc5f8732f4ec35df452386a2
3mlnssaco <ehfkehfk07@naver.com>
2025-06-09 23:12:49 +0900

    ,

---
+++ first.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier : MIT
+pragma solidity >= 0.8.0 < 0.9.0;
+pragma solidity ^0.8.0;
+
+contract solidirty { 
+    // a -> Fun() -> b
+    uint8 publuc a = 10;
+
+    funcrion changeData () public {
+        a=15
+    }
+}
